0a53493cf81c7cee27fc7e7accf79a053e08db0b
方国锋 <beckfun@sina.com>
2018-10-17 11:23:12 +0800

    修改环境地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://192.168.10.11:30812/api"',
-  BASE_EGG_API: process.env.PORTAL_SERVER_BASE_EGG_API || '"http://192.168.10.11:31142/api"',
-  ATTACHMENT_PATH: process.env.PORTAL_ATTACHMENT_PATH || '"http://192.168.10.11:32402/"',
-  SERVER_API: process.env.PORTAL_SERVER_API || '"http://192.168.10.11:31680/api"',
+  BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://10.33.10.206:30618/api"', // datav-server 地址
+  BASE_EGG_API: process.env.PORTAL_SERVER_BASE_EGG_API || '"http://10.33.10.206:31954/api"', // 用户端地址
+  ATTACHMENT_PATH: process.env.PORTAL_ATTACHMENT_PATH || '"http://10.33.10.206:31122/"',// 附件地址
+  SERVER_API: process.env.PORTAL_SERVER_API || '"http://10.33.10.206:31534/api"', // 主项目后端地址
 }
