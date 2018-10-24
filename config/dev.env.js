dc2555430eb303d418ecbc996b4484fe118f6b7a
lith <1156187401@qq.com>
2018-10-24 15:37:57 +0800

    可视化天地图

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  //BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://192.168.10.11:30812/api"',
-  BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://localhost:8080/api"',
+  BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://192.168.10.11:30812/api"',
+  //BASE_API: process.env.PORTAL_WEB_BASE_API || '"http://localhost:8080/api"',
   BASE_EGG_API: process.env.PORTAL_SERVER_BASE_EGG_API || '"http://192.168.10.11:31142/api"',
   ATTACHMENT_PATH: process.env.PORTAL_ATTACHMENT_PATH || '"http://192.168.10.11:32402/"',
   SERVER_API: process.env.PORTAL_SERVER_API || '"http://192.168.10.11:31680/api"',
